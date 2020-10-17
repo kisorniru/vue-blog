b11fda4fe66d6a63686f2b6097d34c7ef236b549
kisorniru <kisorniru@gmail.com>
2020-10-18 03:02:11 +0600

    get request is done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <!-- At last use the component here -->
-    <add-blog></add-blog>
+    <!-- <add-blog></add-blog> -->
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 // Improt Coponent files frst 
 import addBlog from './components/addBlog';
+import showBlogs from './components/showBlogs';
 
 export default {
   // Secondly Register Coponent files here
   components: {
     'add-blog': addBlog,
+    'show-blogs': showBlogs
   },
   data () {
     return {
