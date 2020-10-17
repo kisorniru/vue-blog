a76a3018850428390e5259db48117f4ff0a0f374
kisorniru <kisorniru@gmail.com>
2020-10-18 03:55:24 +0600

    mixins

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="show-blogs">
+  <div v-theme:column="'narrow'" id="show-blogs">
     <h1>All blog Articles</h1>
-    <div class="single-blog" v-for="(blog, index) in blogs" :key="index">
-        <h2>{{ blog.title }}</h2>
-        <article>{{ blog.body }}</article>
+    <input type="text" v-model="search" placeholder="search blogs" >
+    <div class="single-blog" v-for="(blog, index) in filteredBlogs" :key="index">
+        <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+        <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
 
 <script>
+import searchMixin from '../mixins/searchMixin';
+
 export default {
     data () {
         return {
-            blogs: []
+            blogs: [],
+            search: '',
         }
     },
     methods: {
@@ -20,10 +24,34 @@ export default {
     },
     created() {
         this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-            console.log(data.body.slice(0, 10));
             this.blogs = data.body.slice(0, 10);
         });
     },
+    computed: {
+        // Apart from this we are using mixin. Because this search filters will be used by another Or multiple component 
+        /*
+        filteredBlogs: function(){
+            return this.blogs.filter((blog) => {
+                return blog.title.match( this.search );
+            });
+        }
+        */
+    },
+    // Custom Filter Locally
+    filters: {
+        'snippet': function(value) {
+            return value.slice(0, 100) + ' ...';
+        }
+    },
+    // Directives Filter Locally
+    directives: {
+        'rainbow': {
+            bind(el, binding, vnode) {
+                el.style.color = '#' + Math.random().toString().slice(2, 8);
+            },
+        }
+    },
+    mixins: [searchMixin]
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -8,6 +8,41 @@ import VueResource from 'vue-resource'
 // Thirdly we are telling vue.js that we want to use vue-resource
 Vue.use(VueResource);
 
+// Custom Directives
+Vue.directive('rainbow', {
+  // here el = <h2 v-rainbow>{{ blog.title }}</h2>
+  // binding = v-rainbow value
+  // argument = v-rainbow: value
+  bind(el, binding, vnode) {
+    el.style.color = '#' + Math.random().toString().slice(2, 8);
+  }
+});
+
+// Custom Directives
+Vue.directive('theme', {
+  // here el = <div v-theme="" id="show-blogs"> ... ... </div>
+  // binding = v-theme value i.e wide / narrow etc.
+  // argument = v-rainbow:column, here column is an argument value
+  bind(el, binding, vnode) {
+
+    if (binding.value == 'wide') {
+      el.style.maxWidth = "1000px";
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = "560px";
+    }
+    if (binding.arg == "column") {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+
+  }
+});
+
+// Custom Filter Globally
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase();
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
